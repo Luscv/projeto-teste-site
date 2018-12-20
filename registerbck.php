12036c8e5f638716d520293ae8e9c0a350b559bd
mocreviz <43018309+mocreviz@users.noreply.github.com>
2018-12-20 19:03:19 -0200

    Update registerbck.php
    aprendi como acessar cada atributo da classe p inserir, testa ai

--- registerbck.php
+++ registerbck.php
@@ -1,52 +1,51 @@
 <?php
-	class user{
-		public $nome;
+	class Usuario{
+  		public $nome;
 		public $faculdade;
-		public $curso;
+  		public $curso;
 		private $email;
 		private $senha;
-		public function criauser($nome, $faculdade, $curso, $email, $senha){
-		}
+		private $verificaemail;
 	}
 
 	include("db.php");
-	
-	public function criauser($nome, $faculdade, $curso, $email, $senha){
-		
-		if (isset($_POST['criar'])) {
-			$nome = $_POST['nome'];
-			$faculdade = $_POST['faculdade'];
-			$curso = $_POST['curso'];
-			$email = $_POST['email'];
-			$senha = $_POST['senha'];
-			$verificaemail = $_POST['verificaemail'];
+
+	function criauser($nome, $faculdade, $curso, $email, $senha){
+  		$newuser = new Usuario();
+  		if (isset($_POST['criar'])) {
+			$newuser->nome = $_POST['nome'];
+			$newuser->faculdade = $_POST['faculdade'];
+			$newuser->curso = $_POST['curso'];
+			$newuser->email = $_POST['email'];
+			$newuser->senha = $_POST['senha'];
+			$newuser->verificaemail = $_POST['verificaemail'];
 	
 
 			$checar_email = mysql_query("SELECT email FROM users WHERE email='$email'");				
 			$checagem_email = mysql_num_rows($checar_email);
 			if ($checagem_email >= 1) {
 
-				echo '<h3>VocÍ j· est· cadastrado no nosso mundo! FaÁa login clicando <a href="login.php">aqui</a></h3>';
+				echo '<h3>Voc√™ j√° est√° cadastrado no nosso mundo! Fa√ßa login clicando <a href="login.php">aqui</a></h3>';
 
 			}elseif ($email!= $verificaemail) {
 
-				echo '<h3>Os e-mails que vocÍ digitou n„o s„o iguais :(</h3>';
+				echo '<h3>Os e-mails que voc√™ digitou n√£o s√£o iguais :(</h3>';
 
 			}elseif ($nome == '' OR strlen($nome)<4) {
 
-				echo '<h3>Nosso BOT acha que esse n„o È o seu nome :(</h3>';
+				echo '<h3>Nosso BOT acha que esse n√£o √© o seu nome :(</h3>';
 
 			}elseif ($faculdade == '' OR strlen($faculdade)<2) {
 
-				echo '<h3>Nosso BOT pesquisou atÈ mesmo no gling, e n„o achou essa faculdade :(</h3>';
+				echo '<h3>Nosso BOT pesquisou at√© mesmo no gling, e n√£o achou essa faculdade :(</h3>';
 
 			}elseif ($curso == '' OR strlen($curso)<4) {
 
-				echo '<h3>Nosso BOT n„o encontrou seu curso no nosso amplo servidor :( Que tal trocar de curso? xD</h3>';
+				echo '<h3>Nosso BOT n√£o encontrou seu curso no nosso amplo servidor :( Que tal trocar de curso? xD</h3>';
 
 			}elseif ($email == '' OR strlen($email)<10) {
 
-				echo '<h3>Nosso BOT acha que esse n„o È o seu e-mail :(</h3>';
+				echo '<h3>Nosso BOT acha que esse n√£o √© o seu e-mail :(</h3>';
 
 			}elseif ($senha == '' OR strlen($senha)<5) {
 
@@ -64,7 +63,7 @@
 
 				}else{
 
-					echo "<h3>Ops! Algo errado n„o est· certo :(</h3>";
+					echo "<h3>Ops! Algo errado n√£o est√° certo :(</h3>";
 
 				}
 			}
@@ -72,7 +71,7 @@
 	}
 ?>
 
-<!-- fim do SCRIPT php para realizar a operaÁ„o de verificaÁ„o e efetuaÁ„o do cadastro-->
+<!-- fim do SCRIPT php para realizar a opera√ß√£o de verifica√ß√£o e efetua√ß√£o do cadastro-->
 	
 
 	
